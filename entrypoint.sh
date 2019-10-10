787b1ccb23216df60327f2d2fb122305ae13c03b
Brian Douglas <bdougie@users.noreply.github.com>
2019-10-10 09:07:38 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,11 @@ fi
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
 
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git remote add fork "${remote_repo}"
+echo "REMOTE: ${remote_repo}"
+git show-ref # useful for debugging
+
 # inspired by https://dev.to/ranewallin/how-to-keep-your-forked-repository-current-38mn
 
 # If you haven't already added an upstream source, set your upstream
@@ -23,4 +28,7 @@ git remote -v
 git fetch upstream
 
 # Merge the branches and commits from the upstream
+git checkout master
 git merge upstream/master -v
+
+git push fork master
